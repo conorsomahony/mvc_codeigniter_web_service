aa1af871107cc83625f26766afd77de3a72fe1ea
Conor O Mahony <conor.s.omahony@gmail.com>
2019-01-07 12:56:53 +0000

    development folder in config

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost/src/mvc/assessment_1/';
-$config['base_url'] = 'https://www.conorsomahony.net/sites/mvc-web-service/';
+//$config['base_url'] = 'https://www.conorsomahony.net/sites/mvc-web-service/';
 
 /*
 |--------------------------------------------------------------------------
